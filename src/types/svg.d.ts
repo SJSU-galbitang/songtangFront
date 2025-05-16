77ffc1b16abb02f77f0a386bf4eed4f86de92af7
zuu3 <ojh27467242@gmail.com>
2025-05-16 23:44:31 +0900

    refactor(#10) :: svg.d.ts 제거

--- src/types/svg.d.ts
+++
@@ -1,6 +0,0 @@
-declare module '*.svg' {
-  import { ComponentType, SVGProps } from 'react';
-  export const ReactComponent: ComponentType<SVGProps<SVGSVGElement>>;
-  const src: string;
-  export default src;
-}
