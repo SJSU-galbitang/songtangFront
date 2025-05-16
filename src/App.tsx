6d8412424a14c555020ea2b14b2babba7f520f70
zuu3 <ojh27467242@gmail.com>
2025-05-16 11:24:46 +0900

    feat(#11) :: 라우팅 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 /** @jsxImportSource @emotion/react */
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
-import LandingPage from '@/pages/LandingPage.tsx';
+import MainPage from './pages/mainPage';
+import Result from './pages/result';
+import Survey from './pages/survey';
 
 function App() {
   return (
     <>
-      <LandingPage />
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/survey" element={<Survey />} />
+        <Route path="/result" element={<Result />} />
+      </Routes>
     </>
   );
 }
